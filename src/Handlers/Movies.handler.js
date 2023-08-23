a0836059bb2c3b824eb93535649f1571525b1d98
Marian-Sofia <sofiagutierrez0031@gmail.com>
2023-08-23 17:42:34 -0500

    m

---
+++ src/Handlers/Movies.handler.js
@@ -0,0 +1,24 @@
+const getAllMovieHandler = (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+const getActiveMovieHandler = (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }  
+}
+
+const getIdMovieHandler = (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }  
+}
+
--- src/Router/Movies.routes.js
+++ src/Router/Movies.routes.js
@@ -1,7 +1,20 @@
 const { Router } = require("express");
+const {
+  getAllMovieHandler,
+  getActiveMovieHandler,
+  getIdMovieHandler,
+  getNameMovieHandler,
+} = require("../Handlers/Movies.handler");
 
 const router = Router();
 
-//
+// GET DE TODAS LAS PELICULAS
+router.get("/all", getAllMovieHandler);
+
+// GET DE PELICULAS ACTIVAS
+router.get("/", getActiveMovieHandler);
+
+// GET DE LAS PELICULAS POR ID
+router.get("/:movieId", getIdMovieHandler);
 
 module.exports = router;
