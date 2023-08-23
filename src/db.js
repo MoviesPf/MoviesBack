ff1fb39af0f7b2af932a28a06af1d1943ee63bd5
jaider-nieto <jaiderlolxd@gmail.com>
2023-08-22 23:58:43 -0500

    s

--- src/db.js
+++ src/db.js
@@ -1,7 +1,5 @@
 const { DB, DB_PASSWORD, DB_USER, DB_HOST } = require("dotenv").config().parsed;
 
-console.log(DB);
-
 const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(DB, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
