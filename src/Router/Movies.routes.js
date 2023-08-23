1ff47789a8d30e5497eae7dc814fca208fa09a93
jaider-nieto <jaiderlolxd@gmail.com>
2023-08-23 17:34:49 -0500

    moviesHandler

--- src/Router/Movies.routes.js
+++ src/Router/Movies.routes.js
@@ -1,7 +1,19 @@
 const { Router } = require("express");
+const {
+  createMovieHandler,
+  deleteMovieHandler,
+  updateMovieHandler,
+} = require("../Handlers/Movies.handler");
 
 const router = Router();
 
-//
+// POST - CREAR PELICULAS
+router.post("/", createMovieHandler);
+
+// PATCH - EDITAR PELICULAS
+router.patch("/", updateMovieHandler);
+
+// DELETE - BANEAR PELICULAS
+router.delete("/", deleteMovieHandler);
 
 module.exports = router;
