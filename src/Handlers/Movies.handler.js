7b2edcc8bb0ac643363a0731a83383d49012eb15
Marian-Sofia <sofiagutierrez0031@gmail.com>
2023-08-23 17:49:33 -0500

    handlers

--- src/Handlers/Movies.handler.js
+++ src/Handlers/Movies.handler.js
@@ -1,32 +1,44 @@
-const getAllMovieHandler = (req, res) => {
+const getAllMovieHandler = (req, res, next) => {
   try {
-    
   } catch (error) {
-    
+    next(error);
   }
-}
+};
 
-const getActiveMovieHandler = (req, res) => {
+const getActiveMovieHandler = (req, res, next) => {
   try {
-    
   } catch (error) {
-    
-  }  
-}
+    next(error);
+  }
+};
 
-const getIdMovieHandler = (req, res) => {
+const getIdMovieHandler = (req, res, next) => {
   try {
-    
   } catch (error) {
-    
-  }  
-}
+    next(error);
+  }
+};
 
-const createMovieHandler = (req, res) => {};
+const createMovieHandler = (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
 
-const updateMovieHandler = (req, res) => {};
+const updateMovieHandler = (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
 
-const deleteMovieHandler = (req, res) => {};
+const deleteMovieHandler = (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   getAllMovieHandler,
