b7daafe24e24411d6fc3f33a585f0b7519ff099d
jaider-nieto <jaiderlolxd@gmail.com>
2023-08-22 22:44:34 -0500

    models db

--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
 require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const sequelize = require("./db");
-const router = require('./Router/index.js');
-const port = process.env.PORT || 3001
+const router = require("./Router/index.js");
+const port = process.env.PORT || 3001;
 
-const app = express()
-
+const app = express();
 
 // MIDDLEWARES
 app.use(express.json());
 app.use(cors());
 
-app.use('/', router)
+app.use("/", router);
 
-
-sequelize.sync({force: false})
+sequelize
+  .sync({ force: false })
   .then(() => {
     app.listen(port, () => {
-      console.log("Server running on 3001")
-    })
+      console.log("Server running on 3001");
+    });
   })
   .catch((error) => {
-    console.log(error)
-  })
-
+    console.log(error);
+  });
 
 app.use((err, req, res, next) => {
-    const status = err.status || 500;
-    const message = err.message || err;
-    console.error(err);
-    res.status(status).send(message);
-  });
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
