8652d4d960d28760594a412c2268089330054b1a
CabreraBrian <cabrerabriantbj@gmail.com>
2023-08-29 18:33:14 -0300

    next y pequeñas mejoras

--- src/Handlers/Users.handler.js
+++ src/Handlers/Users.handler.js
@@ -1,6 +1,12 @@
-const { createUser, getAllUsers, findUserById, banUserById, userEdit } = require('../Controllers/Users.controller.js');
+const { 
+    createUser, 
+    getAllUsers, 
+    findUserById, 
+    banUserById, 
+    userEdit 
+} = require('../Controllers/Users.controller.js');
 
-const postUser = async (req, res) => {
+const postUser = async (req, res, next) => {
     try {
         const { name, nickname, avatar, email, password, status} = req.body;
         if(!name || !nickname || !avatar || !email || !password || !status) return res.status(400).send('Faltan datos');
@@ -8,40 +14,40 @@ const postUser = async (req, res) => {
         const user = await createUser(name, nickname, avatar, email, password, status);
         res.status(200).json(user);
     } catch (error) {
-        res.status(400).json({error: error.message});
-    }
-}
+        next(error);
+    };
+};
 
-const getUsers = async (req, res) => {
+const getUsers = async (req, res, next) => {
     try {
         const allUsers = await getAllUsers();
         res.status(200).json(allUsers);
     } catch (error) {
-        res.status(400).json({error: error.message});
-    }
-}
+        next(error);
+    };
+};
 
-const getUserById = async (req, res) => {
+const getUserById = async (req, res, next) => {
     const {id} = req.params;
     try {
         const userById = await findUserById(id);
         res.status(200).json(userById)
     } catch (error) {
-        res.status(400).json({error: error.message});
-    }
-}
+        next(error);
+    };
+};
 
-const banDesbanUser = async (req, res) => {
+const banDesbanUser = async (req, res, next) => {
     const {id} = req.params;
     try {
         const bannedUser = await banUserById(id);
         res.status(200).json(bannedUser)
     } catch (error) {
-        res.status(400).json({error: error.message});
-    }
-}
+        next(error);
+    };
+};
 
-const editUser = async (req, res) => {
+const editUser = async (req, res, next) => {
     try {
         const {id} = req.params;
         const body = req.body;
@@ -51,9 +57,9 @@ const editUser = async (req, res) => {
         const editedUser = await userEdit(id, body)
         return res.status(200).json(editedUser)
     } catch (error) {
-        res.status(400).json({ error: error.message})
-    }
-}
+        next(error);
+    };
+};
 
 module.exports = {
     postUser,
