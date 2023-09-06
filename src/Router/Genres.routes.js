82d2a650d569f85fcabec7494bca0dfa3aa84346
CabreraBrian <cabrerabriantbj@gmail.com>
2023-09-06 02:10:26 -0300

    rutas para generos por movie/serie

--- src/Router/Genres.routes.js
+++ src/Router/Genres.routes.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 const {
   getGenresHandler,
+  getMoviesGenresHandler,
+  getSeriesGenresHandler,
   createGenreHandler,
   deleteGenreHandler,
 } = require("../Handlers/Genres.handler.js");
@@ -8,6 +10,8 @@ const {
 const router = Router();
 
 router.get("/", getGenresHandler);
+router.get("/movies", getMoviesGenresHandler);
+router.get("/series", getSeriesGenresHandler);
 router.post("/", createGenreHandler);
 router.delete("/:id", deleteGenreHandler);
 
