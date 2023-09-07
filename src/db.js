c01a652f6173bfd282d6b5138f409d08e5b7d42f
gabr3924 <garc2004@gmail.com>
2023-09-07 14:43:26 -0400

    deployBack

--- src/db.js
+++ src/db.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.POSTGRES_URL + "?sslmode=require", {
-  dialect: 'postgres', // El dialecto depende de tu base de datos
-  logging: false, // Puedes habilitar esto para ver registros de consulta en la consola
+  dialect: 'postgres', 
+  logging: false, 
 });
 
 // Manejador de eventos para verificar si la conexión se ha establecido correctamente.
@@ -17,20 +17,3 @@ sequelize
   });
 
 module.exports = sequelize;
-
-
-// require('dotenv').config();
-// const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_DATABASE } =
-//   process.env;
-
-// const { Sequelize } = require('sequelize');
-
-// const sequelize = new Sequelize(
-//   `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DATABASE}`,
-//   {
-//     logging: false,
-//     native: false
-//   }
-// );
-// // 
-// module.exports = sequelize;
