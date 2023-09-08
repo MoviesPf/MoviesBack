5e3e53d81d3cffbadd067ded6692792df28350e6
dannagonim <dannagonim@gmail.com>
2023-09-08 20:03:59 -0300

    changes

--- src/Handlers/Donations.handler.js
+++ src/Handlers/Donations.handler.js
@@ -1,11 +1,12 @@
 const mercadopago = require("mercadopago");
-
+const Donations = require('../Models/Donations.model');
 const createOrder = async (req, res) => {
   try {
     mercadopago.configure({
       access_token: "TEST-8904276648822814-090619-7c57530ea001e5648d390ac642516a24-1472539992"
     })
-
+    const successMessage = "¡Muchas Gracias! Su donación se ha realizado con éxito";
+    const failureMessage = "¡Oh! ha ocurrido un error al realizar su donación, vuelve a intentarlo";
     const popcorn = req.body;
     const result = await mercadopago.preferences.create({
       items: [
@@ -16,14 +17,25 @@ const createOrder = async (req, res) => {
           quantity: parseInt(popcorn.quantity, 10) ?? 0,
         }],
       back_urls: {
-        success: "http://localhost:3001/donations/success",
-        failure: "http://localhost:3001/donations/failure",
-        pending: "http://localhost:3001/donations/pending",
+        success: `http://localhost:5173/donate?message=${encodeURIComponent(successMessage)}`,
+        failure: `http://localhost:5173/donate?message=${encodeURIComponent(failureMessage)}`,
+        pending: "http://localhost:5173/donate",
       },
       auto_return: "approved",
     });
 
     console.log(result);
+
+    // si fue exitosa la donacion se guarda en la base de datos
+    if (result && result.response && result.response.id) {
+      console.log('Antes de la creación del registro en la base de datos');
+      const newDonation = await Donations.create({
+        date: new Date(),
+        amount: parseInt(popcorn.price, 10),
+      });
+      console.log('Después de la creación del registro en la base de datos');
+      console.log('Datos de donación guardados en la base de datos:', newDonation.toJSON());
+    }
     res.status(200).json({ response: result });
   } catch (error) {
     console.error(error);
@@ -31,36 +43,6 @@ const createOrder = async (req, res) => {
   }
 }
 
-const getSuccess = async (req, res) => {
-  try {
-    res.status(200).json({ message: "Su donación se ha realizado con éxito" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-const getFailure = async (req, res) => {
-  try {
-    res.status(200).json({ message: "Oh, subo un error al realizar su donación" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-
-const getPending = async (req, res) => {
-  try {
-    res.status(200).json({ message: "Su donación esta en pendiente" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-
-
 module.exports = {
-  createOrder,
-  getSuccess,
-  getFailure,
-  getPending,
+  createOrder
 };
--- src/Router/Donations.routes.js
+++ src/Router/Donations.routes.js
@@ -1,12 +1,9 @@
 const { Router } = require('express');
-const { createOrder, getSuccess, getFailure, getPending } = require('../Handlers/Donations.handler');
+const { createOrder } = require('../Handlers/Donations.handler');
 
 const router = Router();
 
 router.post('/create-order', createOrder);
-router.get('/success', getSuccess);
-router.get('/failure', getFailure);
-router.get('/pending', getPending);
 router.post('/webhook', (req, res) => {
   console.log(req.body);
   res.status(200).send('Webhook received');
