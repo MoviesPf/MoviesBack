b92528caf78b97ba3211f7afe38952f2dea6f516
dannagonim <dannagonim@gmail.com>
2023-09-11 18:11:48 -0300

    adadada

--- src/Controllers/reviews.controller.js
+++ src/Controllers/reviews.controller.js
@@ -1,5 +1,5 @@
-const Reviews = require("../Models/Reviews.model");
-const Users = require("../Models/Users.model");
+const Reviews = require('../Models/Reviews.model');
+const Users = require('../Models/Users.model');
 
 const getAllReviews = async () => {
   // La funcion devuelve todas las reviews en la db.
@@ -11,25 +11,21 @@ const getUserReviews = async (userID) => {
   // La funcion devuelve todas las reviews del usuario con el id pasado por parametro.
   const reviews = await Reviews.findAll({ where: { UserId: userID } });
   if (!reviews) {
-    throw new Error({ msg: "Hubo un problema al buscar las reviews", error });
+    throw new Error({ msg: 'Hubo un problema al buscar las reviews', error });
   }
   return reviews;
 };
 
-const createReview = async (userID, reviewData, ProgramId) => {
+const createReview = async (userID, reviewData) => {
   // La funcion crea una nueva review y le asigna un usuario relacionado.
 
   const user = await Users.findByPk(userID);
   if (!user) {
-    throw new Error("El ID brindado no coincide con ningun usuario");
+    throw new Error('El ID brindado no coincide con ningun usuario');
   }
-  const newReview = await Reviews.create({
-    ...reviewData,
-    UserId: user.id,
-    ProgramId,
-  });
+  const newReview = await Reviews.create({ ...reviewData, UserId: user.id });
   if (!newReview) {
-    throw new Error({ msg: "Hubo un problema al crear la review", error });
+    throw new Error({ msg: 'Hubo un problema al crear la review', error });
   }
   return newReview;
 };
@@ -44,8 +40,8 @@ const editReview = async (reviewID, newCommentsData) => {
   const reviewToEdit = await Reviews.findByPk(reviewID);
   if (!reviewToEdit) {
     throw new Error({
-      msg: "Hubo un problema al intentar editar la review",
-      error,
+      msg: 'Hubo un problema al intentar editar la review',
+      error
     });
   }
   reviewToEdit.comments = newCommentsData;
@@ -58,5 +54,5 @@ module.exports = {
   createReview,
   getUserReviews,
   deletReview,
-  editReview,
+  editReview
 };
