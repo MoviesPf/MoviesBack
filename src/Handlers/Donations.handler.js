d8b71e71616b0001c594cc72846b4d721b0ea2fb
JuanIFerrando <teritojr@gmail.com>
2023-09-20 17:09:39 -0300

    Prueba Donaciones

--- src/Handlers/Donations.handler.js
+++ src/Handlers/Donations.handler.js
@@ -1,40 +1,40 @@
-const mercadopago = require('mercadopago');
-const Donations = require('../Models/Donations.model');
+const mercadopago = require("mercadopago");
+const Donations = require("../Models/Donations.model");
 const Users = require("../Models/Users.model");
 
 const createOrder = async (req, res) => {
   try {
     mercadopago.configure({
       access_token:
-        'TEST-8904276648822814-090619-7c57530ea001e5648d390ac642516a24-1472539992'
+        "TEST-8904276648822814-090619-7c57530ea001e5648d390ac642516a24-1472539992",
     });
     const successMessage =
-      'Thank you very much! Your donation has been successfully completed';
+      "Thank you very much! Your donation has been successfully completed";
     const failureMessage =
-      'Oh! an error occurred while making your donation, please try again.';
+      "Oh! an error occurred while making your donation, please try again.";
     const popcorn = req.body;
 
-    const user = await Users.findByPk(popcorn.userId)
-    console.log(user)
+    const user = await Users.findByPk(popcorn.userId);
+    console.log(user);
     const result = await mercadopago.preferences.create({
       items: [
         {
           title: popcorn.title,
-          currency_id: 'ARS',
+          currency_id: "ARS",
           unit_price: Number(popcorn.price),
-          quantity: parseInt(popcorn.quantity, 10) ?? 0
-        }
+          quantity: parseInt(popcorn.quantity, 10) ?? 0,
+        },
       ],
       back_urls: {
-        success: `http://localhost:5173/donate?message=${encodeURIComponent(
+        success: `https://movies-front-tau.vercel.app/donate?message=${encodeURIComponent(
           successMessage
         )}`,
-        failure: `http://localhost:5173/donate?message=${encodeURIComponent(
+        failure: `https://movies-front-tau.vercel.app/donate?message=${encodeURIComponent(
           failureMessage
         )}`,
-        pending: 'http://localhost:5173/donate'
+        pending: "https://movies-front-tau.vercel.app/donate",
       },
-      auto_return: 'approved'
+      auto_return: "approved",
     });
 
     console.log(result);
@@ -44,27 +44,27 @@ const createOrder = async (req, res) => {
       user.donator = true;
       user.save();
 
-      console.log('Antes de la creación del registro en la base de datos');
+      console.log("Antes de la creación del registro en la base de datos");
       const newDonation = await Donations.create({
         date: new Date(),
         amount: parseInt(popcorn.price, 10),
         UserId: user.id,
       });
-      console.log('Después de la creación del registro en la base de datos');
+      console.log("Después de la creación del registro en la base de datos");
       console.log(
-        'Datos de donación guardados en la base de datos:',
+        "Datos de donación guardados en la base de datos:",
         newDonation.toJSON(),
-        "user id", 
-        user.id,
+        "user id",
+        user.id
       );
     }
     res.status(200).json({ response: result });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Ocurrió un error' });
+    res.status(500).json({ error: "Ocurrió un error" });
   }
 };
 
 module.exports = {
-  createOrder
+  createOrder,
 };
